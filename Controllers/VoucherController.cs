2352ea9f08de6d8becc8ac279156742bc850436e
VN The Boss Baby <21520178@gm.uit.edu.vn>
2024-05-31 11:00:48 +0700

    first commit

---
+++ Controllers/VoucherController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.DTOs;
+using PetShop.Services.VoucherService;
+
+namespace PetShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VoucherController : ControllerBase
+    {
+        private readonly IVoucherService _voucherService;
+        public VoucherController(IVoucherService voucherService)
+        {
+            _voucherService = voucherService;
+        }
+
+        [HttpPost("create")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create([FromBody] VoucherDto request)
+        {
+            return await _voucherService.Create(request);
+        }
+
+        [HttpGet("all")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllAdmin()
+        {
+            return await _voucherService.GetAllAdmin();
+        }
+
+        [HttpDelete("delete/{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            return await _voucherService.Delete(id);
+        }
+
+        [HttpGet("get/{code}")]
+        //[Authorize]
+        public async Task<IActionResult> GetCode([FromRoute] string code)
+        {
+            return await _voucherService.GetCode(code);
+        }
+        [HttpGet("apply/{code}")]
+        public async Task<IActionResult> ApplyCode([FromRoute] string code)
+        {
+            return await _voucherService.ApplyCode(code);
+        }
+        [HttpGet("list")]
+        public async Task<IActionResult> List()
+        {
+            return await _voucherService.List();
+        }
+    }
+}
---
+++ Services/CommentService/ICommentService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.DTOs;
+
+namespace PetShop.Services.CommentService
+{
+    public interface ICommentService
+    {
+        Task<IActionResult> Create(CommentDto request);
+        /*Task<IActionResult> GetCommentProduct(int product_id, Comment2Dto request);*/
+        Task<IActionResult> GetCommentProduct(int product_id);
+        Task<IActionResult> GetAll();
+        Task<IActionResult> Update(int id, Comment1Dto request);
+    }
+}
